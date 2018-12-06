93606f19c4cabe2db4416593fe3fc3a985d3d017
nico <nicolas.p.suarez@gmail.com>
2018-12-06 14:57:20 -0300

    Remove stages 3,4 and 5

--- stage4/02-extras/00-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-#Alacarte fixes
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
